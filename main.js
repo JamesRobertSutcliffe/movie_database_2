b9a5e5ad56999162fc37e806f0ce5b1875f04454
“James <jamessutcliffe2012@hotmail.co.uk>
2022-11-10 17:30:52 +0000

    Constructor created new users/sort functions fixed

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ let movieDataKeys = Object.keys(movieData);
 let movieDataEntries = Object.entries(movieData);
 let movieDataValues = Object.values(movieData);
 
-console.log()
+
 console.log()
 // below code renders the movie data object to the html page //
 
@@ -37,7 +37,7 @@ function displayAllMovies(moviesArr) {
 displayAllMovies(movieDataEntries)
 
 
-// Below code re-renders movie database sorted by rating / runtime / release date // find out how to sort through new arrays //
+// Below code re-renders movie database sorted by rating / runtime / release date // 
 
 function sortByRating() {
   const sortedArr = movieDataEntries.sort(function (a, b) {
@@ -72,7 +72,19 @@ function sortByYear() {
 const sortYearBtn = document.getElementById('btn3')
 sortYearBtn.addEventListener('click', sortByYear)
 
-// New Movie function allows the creation new object by user // re-edit newMoveie function using constructor?
+// New Movie function allows the creation new object by user using a constructor, then concatenating this new object onto the movie name and pushing
+// to our source array of objects //
+
+
+class newMoveEntry {
+  constructor(plot, cast, year, runtime, rating) {
+    this.plot = plot;
+    this.cast = cast;
+    this.year = year;
+    this.runtime = runtime;
+    this.rating = rating;
+  }
+}
 
 function newMovie(){
   const newName = document.getElementById("mName").value
@@ -82,11 +94,16 @@ function newMovie(){
   const newRuntime = document.getElementById("runtime").value
   const newRating = document.getElementById("rating").value
   
-  let newMovieDataObj = {'name': newName, 'plot': newPlot, 'cast': newCast, 'year': newYear, 'runtime': newRuntime, 'rating': newRating}
-  movieDataEntries.push(newMovieDataObj)
-  displayMovie (newMovieDataObj)
+  const newMoveEntryData = new newMoveEntry(newPlot, newCast, newYear, newRuntime, newRating);
+  const newMovieArray = [newName, newMoveEntryData]
+  movieDataEntries.push(newMovieArray)
+  displayMovie(newMovieArray)
     
-  console.log(movieData)
+  // let newMovieDataObj = {'name': newName, 'plot': newPlot, 'cast': newCast, 'year': newYear, 'runtime': newRuntime, 'rating': newRating}
+  // movieDataEntries.push(newMovieDataObj)
+  // displayMovie (newMovieDataObj)
+    
+  // console.log(movieData)
    }
 
   const submitNewMovie = document.getElementById("newMovieSubmit");
